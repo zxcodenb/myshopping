b0570be920e8287add21bd4f3dc56929ec959c75
zxcodenb <481269335@qq.com>
2023-09-15 01:30:38 +0800

    修改了项目名

--- shoopingweb-back/src/main/java/com/zx/controller/ProductController.java
+++ shoopingweb-back/src/main/java/com/zx/controller/ProductController.java
@@ -3,7 +3,7 @@ package com.zx.controller;
 
 import com.zx.ProductService;
 import com.zx.pojo.ResultData;
-import com.zx.pojo.ResultData;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,10 +17,10 @@ public class ProductController {
 
 
     @GetMapping
-    public ResultData selAll(@RequestParam(defaultValue = "1") Integer pageNum, @RequestParam(defaultValue = "5") Integer pageSize){
+    public ResultData selAll(@RequestParam(defaultValue = "1") Integer pageNum, @RequestParam(defaultValue = "5") Integer pageSize) {
 
 
-        System.out.println(pageNum+"=="+pageSize);
+        System.out.println(pageNum + "==" + pageSize);
 
         ResultData resultDate = productService.selectByPage(pageNum, pageSize);
 
