ac6cf005747a5e2f5d0f364cab6f16c68bc76900
zxcodenb <481269335@qq.com>
2023-09-21 16:14:03 +0800

    todo

--- mapper/src/main/java/com/zx/mapper/CategoryMapper.java
+++ mapper/src/main/java/com/zx/mapper/CategoryMapper.java
@@ -2,9 +2,11 @@ package com.zx.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zx.pojo.Category;
-
+import com.zx.pojo.ResultData;
 
 import java.util.List;
+
+import javax.naming.spi.DirStateFactory.Result;
 
 
 public interface CategoryMapper extends BaseMapper<Category> {
